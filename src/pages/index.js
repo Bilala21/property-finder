3a2ffb75794691f8f52fc14147240ed93d29732c
M Bilal <bilal.kalri@gmail.com>
2023-02-20 22:43:59 +0500

    fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(()=>{
+    fetch("https://property-finder-three.vercel.app/").then(res=> res.json())
+    .then(data=> console.log(data))
+  },[])
   return (
     <>
       <Head>
