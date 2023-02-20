69a23b634cf251cf6c2e50eef421c5f0e3eb7300
M Bilal <bilal.kalri@gmail.com>
2023-02-21 02:08:40 +0500

    products

--- src/pages/api/get-products.js
+++ src/pages/api/get-products.js
@@ -1,10 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import initDb from "helpers/initDb"
 import Product from "models/Product"
 
+initDb()
 
 export default function  handler(req, res) {
   const resdata = Product.find({}).lean()
 
-  res.status(200).json({products })
+  res.status(200).json({resdata })
 }
