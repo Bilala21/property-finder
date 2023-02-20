ba7072d43149e46f79907003f4769c4dce6f2453
M Bilal <bilal.kalri@gmail.com>
2023-02-20 23:58:31 +0500

    dfdfdfhhhh

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,12 @@ export default function Home() {
   const [proData, serProData] = useState([])
   useEffect(()=>{
     getData()
+    console.log("fecthing")
   },[])
   const getData = async ()=>{
     const res = await fetch("https://property-finder-three.vercel.app/api/get-products")
     const data= await res.json()
     serProData(data.data)
-
-    console.log(proData)
   }
   return (
     <>
